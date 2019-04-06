889a315b46deeb12d9986b8e11045ce10c976565
Glitch (nmorin86-boilerplate-project-timestamp) <none>
2019-04-06 02:04:21 +0000

    🐵🕶 Checkpoint ./server.js:3676277/2 ./timestamp.js:3676277/389

--- timestamp.js
+++ timestamp.js
@@ -1,10 +1,24 @@
 let express = require('express');
 
 function sendStamp(req, res, next) {
-  let timeIn = req.params.date_string;
+  let timeIn = req.path.split('/')[3];
   console.log("In sendstamp: ", timeIn);
-  res.send("Hello there, " + timeIn);
-  next();
+  let time = parseToDate(timeIn);
+  let JSONout = parseToJSON(time);
+  res.json(JSONout);
+  next();  
+}
+
+function parseToDate(timeIn) {
+  let date;
+  if(timeIn === '') {
+    date = new Date();
+  } else {
+    try {
+      
+}
+
+function parseToJSON(time) {
   
 }
 
