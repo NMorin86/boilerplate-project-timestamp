aeed41f6d4f44e52fcd3d381beb33d59ed1f823c
Glitch (nmorin86-boilerplate-project-timestamp) <none>
2019-04-06 02:15:21 +0000

    🍝🚿 Checkpoint ./timestamp.js:3676277/459

--- timestamp.js
+++ timestamp.js
@@ -11,15 +11,30 @@ function sendStamp(req, res, next) {
 
 function parseToDate(timeIn) {
   let date;
+  
+  // Trim leading 0s
+  timeIn = timeIn.replace(/^0+/, '')
+  
   if(timeIn === '') {
     date = new Date();
+  } else if(String(parseInt(timeIn)).length == timeIn.length) {
+    // We have a UNIX number
+    date = new Date(parseInt(timeIn));
   } else {
-    try {
-      
+    // Try a regular parse
+    date = new Date(timeIn)
+  }
+  console.log(date);
+  
+  return date;
+    
 }
 
 function parseToJSON(time) {
-  
+  let JSON = {};
+  JSON.unix = time.getTime();
+  JSON.utc = time.toString();
+  return JSON;
 }
 
 exports.sendStamp = sendStamp;
